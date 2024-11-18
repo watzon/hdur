96d41ade244f5ab140aa84f2b7c4ea48b0fd87a7
Chris Watson <cawatson1993@gmail.com>
2024-11-18 01:49:04 -0700

    ✨ feat: add ToValue function to convert duration string to reflect.Value

--- convert.go
+++ convert.go
@@ -1,6 +1,9 @@
 package hdur
 
-import "time"
+import (
+	"reflect"
+	"time"
+)
 
 // ToStandard converts our Duration to a time.Duration
 // Note: This is an approximation as time.Duration doesn't handle months and years
@@ -8,4 +11,13 @@ func (d Duration) ToStandard() time.Duration {
 	// Convert everything to nanoseconds
 	t := time.Now()
 	return d.Add(t).Sub(t)
+}
+
+// ToValue converts a duration string to a reflect.Value, perfect for use
+// with Fiber
+func ToValue(value string) reflect.Value {
+	if v, err := ParseDuration(value); err == nil {
+		return reflect.ValueOf(v)
+	}
+	return reflect.Value{}
 }
